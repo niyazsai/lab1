7e9f0b3284c68717a6b1cda96e4186e76025dd9b
niyaz sai <niyazsajfullin@gmail.com>
2024-11-09 13:58:45 +0300

    1

---
+++ lab1/Program.cs
@@ -0,0 +1,12 @@
+namespace lab1;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var repository = new ShoppingListRepository(new JsonSerializer());
+        var commandHandler = await CommandHandler.Initialize(repository);
+        var ui = new UserInterface(repository, commandHandler);
+        ui.MainMenu();
+    }
+}
